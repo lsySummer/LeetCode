728a2e79d74db13b972ea5ae0f3554b22a136dde
lsySummer <864986781@qq.com>
2016-09-06 17:49:57 +0800

    更新两题
    异或操作查重

--- src/demo/TreeNode.java
+++ src/demo/TreeNode.java
@@ -5,7 +5,14 @@ public class TreeNode {
 	TreeNode left;
 	TreeNode right;
 	TreeNode(int x) { val = x; }
-	
+	/**
+	 * Given a binary tree, find its maximum depth.
+	 * 
+	 * The maximum depth is the number of nodes along the longest path from the
+	 * root node down to the farthest leaf node.
+	 * 
+	 * Subscribe to see which companies asked this question
+	 */
 
 	public int maxDepth(TreeNode root) {
 		if(root==null){
@@ -14,6 +21,12 @@ public class TreeNode {
 			return 1+Math.max(maxDepth(root.left),maxDepth(root.right ));
 		}
 	}
+	
+    
+    /**invert a binary tree*/
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
 }
 
 
